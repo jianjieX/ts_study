c54e0ef1428dd5378bad8c6c71b4445d3e9a109c
EDZ <jianjie_x@163.com>
2020-01-16 11:45:19 +0800

    ts

---
+++ test_ts/variable.js
@@ -0,0 +1,6 @@
+var myName = 'sjj';
+console.log(myName);
+var score = 1;
+var score2 = 2.1;
+var sum = score + score2;
+console.log(sum);
---
+++ test_ts/variable.ts
@@ -0,0 +1,6 @@
+var myName:string='sjj';
+console.log(myName);
+var score:number=1;
+var score2:number=2.1;
+var sum=score+score2;
+console.log(sum);
